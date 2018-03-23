ec6672f96ea7bb3b73c51d91c1b9384d340bb3ad
Ira Copperman <ira.copperman@gmail.com>
2018-03-23 14:06:17 -0400

    latest changes

---
+++ src/app/appLoad.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { NsqipService } from './nsqip.service';
+
+@Injectable()
+export class AppLoadSvc {
+
+  constructor(private http: HttpClient, private q: NsqipService) {}
+
+  getURL() {
+
+    let path = 'assets/url.txt' //really a json file, but iis will not serve json files
+
+    let promise = this.http.get(path)
+                  .toPromise()
+                  .then(settings => {
+                    this.q.url = settings['url'];
+                    console.log('here')
+                    return settings
+                  })
+                  .catch( (err) => {
+                    this.http.get('assets/url.json')
+                        .toPromise()
+                        .then(settings => {
+                          this.q.url = settings['url'];
+                          console.log('here')
+                          return settings
+                        })
+                        .catch( (err) => {
+                          console.log('cant find assets/url')
+
+                        })
+
+                  })
+
+    return promise
+
+  }
+}
